a63c6d9d719a83c80f217f2fffd9adb66198a6b4
milad <ahmadmilad698@gmail.com>
2026-04-03 12:23:24 +0200

    fix: removed Item and unnecessary code

--- Maps/MapsApp.swift
+++ Maps/MapsApp.swift
@@ -13,24 +13,10 @@ struct MapsApp: App {
     
     @StateObject private var vm = LocationsViewModel()
     
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             LocationsView()
                 .environmentObject(vm)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
